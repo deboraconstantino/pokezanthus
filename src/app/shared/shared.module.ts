1fcce459b8bb67cd86fc8bdd393c35c36c05d2c0
deboraconstantino <45357007+deboraconstantino@users.noreply.github.com>
2021-04-22 19:39:38 -0300

    Adição header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
-import { MenuComponent } from './components/menu/menu.component';
+import { HeaderComponent } from './components/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    MenuComponent
+    HeaderComponent
   ],
   imports: [
     CommonModule,
@@ -18,7 +18,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule
   ],
   exports: [
-    MenuComponent
+    HeaderComponent
   ]
 })
 export class SharedModule { }
